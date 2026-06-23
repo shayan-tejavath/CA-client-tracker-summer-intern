5ef5a8e029db27a8b42bf582e8e2faff385fcc02
shayantejavath12 <shayantejavath12@gmail.com>
2026-06-23 17:43:32 +0530

    Commit all remaining changes

--- backend/routes/notificationRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import protect from "../middleware/authMiddleware.js";
-import { getNotifications } from "../controllers/notificationController.js";
-
-const router = express.Router();
-router.use(protect);
-router.get("/", getNotifications);
-
-export default router;
